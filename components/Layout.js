29f3d72d0a72fc2eacb6215bfaf5477cdb17958a
임빛나 <devbit4@gmail.com>
2022-03-08 17:43:48 +0900

    sidebar 삭제와 layout 구조 변경

--- components/Layout.js
+++ components/Layout.js
@@ -6,43 +6,10 @@ export default function Layout({ children }) {
   return (
     <>
       <Header></Header>
-      <div className="contents">
-        <div className="sidebar">
-          <Sidebar></Sidebar>
-        </div>
         <div className="main">{children}</div>
-      </div>
       <Footer></Footer>
     
-      <style jsx>{
-        `
-        .contents{
-          display:flex;
-          min-height: 500px;
-        }
-        .sidebar{
-          width:20%;
-          border:1px solid #000;
-        }
-        .main{
-          width:80%;
-          border:1px solid #000;
-        }
-        // <tablet 구간>
-        @media screen and (max-width: 768px){
-          .contents{
-            flex-direction:column;
-          }
-          .sidebar{
-            width:100%;
-          }
-          .main{
-            width:100%;
-          }
-        }
-        `
-      }
-      </style>
+    {/* Layout 스타일링 */}
       
     </>
     
