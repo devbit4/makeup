a989c83c2ea1a42385df20de6490f6daac234705
임빛나 <devbit4@gmail.com>
2022-03-09 18:10:29 +0900

    community section ui 개발 진행 중

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,14 +1,24 @@
 import { useRouter } from "next/router";
 
-export default function Sidebar({menus,title}) {
-    const router = useRouter();
-    const handleClick=(menu)=>{
-        router.push(`/${title}/${menu}`)
-    }
+export default function Sidebar({ menus, title }) {
+  const router = useRouter();
+  const handleClick = (menu) => {
+    router.push(`/${title}/${menu}`);
+  };
 
-    return (
-        <ul className="menus">
-            {menus && menus.map((menu,index)=><li key={index} onClick={()=>{handleClick(menu)}}>{menu}</li>)}
-        </ul>
-    );
-  }
+  return (
+    <ul className="menus">
+      {menus &&
+        menus.map((menu, index) => (
+          <li
+            key={index}
+            onClick={() => {
+              handleClick(menu);
+            }}
+          >
+            - {menu}
+          </li>
+        ))}
+    </ul>
+  );
+}
--- pages/community/[section].js
+++ pages/community/[section].js
@@ -1,71 +1,115 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import Sidebar from "../../components/Sidebar";
 import { useRouter } from "next/router";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faCircle } from "@fortawesome/free-solid-svg-icons";
 
-export default function Faq(){
-    const router = useRouter ();
-    const section = router.query.section;
-    const [problems, setProblems] =useState([]);
-    const comminity=["faq","qna"]
-    
-    const getData = ()=>{
-        fetch(`/dbs/${section}.json`)
-        .then(data=>data.json())
-        .then(json=>setProblems(json.data))
-    }
+export default function Faq() {
+  const router = useRouter();
+  const section = router.query.section;
+  const [problems, setProblems] = useState([]);
+  const comminity = ["faq", "qna"];
 
-    useEffect(()=>{
-       section && getData();
-    },[section])
+  const getData = () => {
+    fetch(`/dbs/${section}.json`)
+      .then((data) => data.json())
+      .then((json) => setProblems(json.data));
+  };
+  const handleClick = (e) => {
+    e.target.closest("article").classList.toggle("off");
+  };
 
-    return(
-        <section className="wrapper">
-            <div className="sidebar">
-                 <h1>All sections</h1>
-                <Sidebar menus={comminity} title={"community"}></Sidebar>
-            </div>
-            <div className="main">
-                <h1>{section}</h1>
-                {
-                    problems.map((problem,index)=>{
-                        return(
-                        <article key={index}>
-                            <div className="faq-question">
-                                <div className="faq-question-front">
-                                    <FontAwesomeIcon icon={faCircle} />
-                                    <h2>{problem.question}</h2>
-                                </div>
-                                <FontAwesomeIcon icon={faChevronDown} />
-                            </div>
-                            <div className="faq-answer">
-                                <div className="faq-answer">
-                                    <h2>{problem.answer}</h2>
-                                </div>
-                            </div>
-                        </article>)
-                    })
-                }
-            </div>
-        {/* community section 스타일링 */}
-        <style jsx>{
-        `
-        .wrapper{
-            display:flex;
-        }
-        .sidebar{
-            width:20%;
-            border:1px solid #000;
-        }
-        .main{
-            width:80%;
-            border:1px solid #000;
-        }
-        `
-        }
-        </style>
-        </section>
-    )
+  useEffect(() => {
+    section && getData();
+  }, [section]);
+
+  return (
+    <section className="section wrapper">
+      <div className="sidebar">
+        <h1>All sections</h1>
+        <Sidebar menus={comminity} title={"community"}></Sidebar>
+      </div>
+      <div className="main">
+        <h1 className="main-title">{section}</h1>
+        {problems.map((problem, index) => {
+          return (
+            <article key={index} className="problem">
+              <div className="question" onClick={handleClick}>
+                <div className="question-front">
+                  <FontAwesomeIcon icon={faCircle} />
+                  <h2>{problem.question}</h2>
+                </div>
+                <div className="question-underbar">
+                  <FontAwesomeIcon icon={faChevronDown} />
+                </div>
+              </div>
+              <div className="answer">
+                <p>{problem.answer}</p>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+      {/* community section 스타일링 */}
+      <style jsx>
+        {`
+          .wrapper {
+            display: flex;
+            padding: 0 10vw;
+          }
+          .sidebar {
+            width: 20%;
+            background-color: #777;
+            padding: 40px 10px;
+            color: #fff;
+          }
+          .main {
+            width: 80%;
+            padding: 40px;
+          }
+          .main-title {
+            font: 500 24px "fredoka";
+            margin-bottom: 50px;
+            padding-bottom: 10px;
+            border-bottom: 1px solid #333;
+          }
+          .problem {
+            margin-bottom: 20px;
+          }
+          .question {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+          }
+          .question-front {
+            display: flex;
+            align-items: center;
+            padding: 10px;
+          }
+          .question-underbar {
+            cursor: pointer;
+            transform: rotate(180deg);
+            transition: all 0.5s;
+          }
+          .question-front h2 {
+            font: 700 16px/1 "roboto";
+            margin-left: 10px;
+          }
+          .answer {
+            padding: 20px 0px 0px 20px;
+          }
+          .answer p {
+            font: 400 12px/1.4 "roboto";
+          }
+          .problem.off .answer {
+            display: none;
+          }
+          .problem.off .question-underbar {
+            transform: rotate(0deg);
+          }
+        `}
+      </style>
+    </section>
+  );
 }
