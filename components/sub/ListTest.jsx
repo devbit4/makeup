cc4f988be864a3b5b3ba515be02df5271709080c
임빛나 <devbit4@gmail.com>
2022-03-10 15:16:10 +0900

    makeup 리스트 viewtype 개발 진행 중

--- components/sub/ListTest.jsx
+++ components/sub/ListTest.jsx
@@ -1,15 +1,17 @@
+import clsx from "clsx";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
-export default function ListTest({ products }) {
+export default function ListTest({ products, viewtype }) {
   const router = useRouter();
+
   const handleItemClick = (id) => {
     router.push(`/view/${id}`);
   };
+
   return (
     <>
-      <input type="checkbox"></input>
-      <input type="checkbox"></input>
-      <ul className="items">
+      <ul className={clsx("items", viewtype === "one" ? "one" : "")}>
         {products &&
           products.map((product) => {
             return (
@@ -18,14 +20,69 @@ export default function ListTest({ products }) {
                 key={product.id}
                 onClick={() => handleItemClick(product.id)}
               >
-                <img src={product.image_link}></img>
-                <strong>{product.name}</strong>
-                <span>{product.product_type}</span>
-                <strong>${product.price}</strong>
+                <div className="item-pic">
+                  <img src={product.image_link} alt="item-img"></img>
+                </div>
+                <div className="item-text">
+                  <strong className="item-name">
+                    {product.name.slice(0, 50)}
+                  </strong>
+                  <span className="item-type">{product.product_type}</span>
+                  <strong className="item-price">${product.price}</strong>
+                  <p className="item-description">
+                    {product.description.slice(0, 100)}...
+                  </p>
+                  <button className="item-btn">상품 더 보기</button>
+                </div>
               </li>
             );
           })}
-        <style jsx>{``}</style>
+        <style jsx>{`
+          .items {
+            padding: 40px 80px;
+            display: flex;
+            flex-wrap: wrap;
+          }
+          .item {
+            width: 50%;
+            display: flex;
+            align-items: center;
+            margin-bottom: 20px;
+            border: 1px solid #ddd;
+          }
+          .items.one .item {
+            width: 100%;
+          }
+          .item-pic {
+            width: 300px;
+            height: 200px;
+          }
+          .item-pic img {
+            width: 100%;
+            height: 100%;
+          }
+          .item-text {
+            padding: 10px;
+          }
+          .item-name {
+            display: block;
+            font: 700 14px/1 "roboto";
+            margin-bottom: 20px;
+          }
+          .item-type {
+            display: block;
+            font: 400 12px/1 "questrial";
+            margin-bottom: 10px;
+          }
+          .item-price {
+            font: 400 24px/1 "questrial";
+            margin-bottom: 10px;
+          }
+          .item-description {
+            font: 400 12px/1 "questrial";
+            margin-bottom: 10px;
+          }
+        `}</style>
       </ul>
     </>
   );
--- pages/makeup/index.jsx
+++ pages/makeup/index.jsx
@@ -1,19 +1,65 @@
+import { useState } from "react";
 import Seo from "../../components/common/Seo";
 import ListTest from "../../components/sub/ListTest";
 
 export default function Makeup({ products }) {
-  console.log(products);
+  const bestItems = products.slice(0, 9);
+  const newItems = products.slice(10, 20);
+  const [viewtype, setViewtype] = useState("two");
+
   return (
     <>
       <Seo title="Makeup"></Seo>
       <div className="makeup">
-        <ListTest products={products}></ListTest>
-        <style jsx>{`
-          .list {
-            padding: 0 12vw;
-          }
-        `}</style>
+        <div className="inner">
+          <ul className="list-type-btns">
+            <li className="list-type-btn">
+              <input
+                onClick={(e) => {
+                  setViewtype("one");
+                }}
+                type="radio"
+                name="gen"
+                id="one"
+                defaultChecked
+              />
+              <label htmlFor="one"></label>
+            </li>
+            <li className="list-type-btn">
+              <input
+                onClick={() => setViewtype("two")}
+                name="gen"
+                type="radio"
+                id="two"
+              />
+              <label htmlFor="two"></label>
+            </li>
+          </ul>
+          <div className="makeup-subtitle">
+            <h2>Best Items</h2>
+          </div>
+          <ListTest products={bestItems} viewtype={viewtype}></ListTest>
+          <div className="makeup-subtitle">
+            <h2>New Items</h2>
+          </div>
+          <ListTest products={bestItems} viewtype={viewtype}></ListTest>
+        </div>
       </div>
+      <style jsx>{`
+        .makeup {
+          width: 100%;
+        }
+        .inner {
+          width: 1180px;
+          margin: 0 auto;
+        }
+        // 반응형
+        @media screen and (max-width: 1180px) {
+          .inner {
+            width: 100%;
+          }
+        }
+      `}</style>
     </>
   );
 }
