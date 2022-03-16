95de5aa49ddde98a67206fa844e76f0ef2622bec
임빛나 <devbit4@gmail.com>
2022-03-16 10:55:56 +0900

    documnet 수정

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,27 +1,46 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+// import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+// class MyDocument extends Document {
+//   static async getInitialProps(ctx) {
+//     const initialProps = await Document.getInitialProps(ctx);
+//     return { ...initialProps };
+//   }
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <meta
-            httpEquiv='Content-Security-Policy'
-            content='upgrade-insecure-requests'
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           <meta
+//             httpEquiv='Content-Security-Policy'
+//             content='upgrade-insecure-requests'
+//           />
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
+
+// export default MyDocument;
+
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta
+          httpEquiv='Content-Security-Policy'
+          content='upgrade-insecure-requests'
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
